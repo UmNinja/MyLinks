941947b26787d355fdad0ee30fcf9b87bd8f45a8
UmNinja <108221987+UmNinja@users.noreply.github.com>
2022-06-27 17:25:08 -0300

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,28 @@
+const config = {
+    
+    USERNAME: "NINJA#6108",
+    /**
+     * This can either be a file link (relative or external)
+     * or a Gravatar email or hash.
+     */
+    PICTURE: "assets/img/profile_picture.png",
+
+    DESCRIPTION: "✨ Ymir's Creator <br>+20 commands<br>",
+
+    BADGES: [
+        { id: "HypeSquad Bravery", image: "assets/img/hypesquad_balance.svg" },
+        { id: "Nitro Sub", image: "assets/img/hypesquad_event.svg" },
+        { id: "Bot Dev", image: "assets/img/verified_bot_developer.svg" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/ymirsupport.png", link: "https://discord.gg/zQBqjNf2VT", name: "🏡 Apartamento da Ymir" },
+        { id: "project-2", image: "assets/img/fazendinha.png", link: "https://discord.com/fazendinha", name: "Fazendinha" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/UmNinja" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/UmNinja" },
+    ],
+
+}
